166532c9ea7d9052257e92909d40b3cfa170dc87
nico <nicomenzii@gmail.com>
2023-03-07 14:31:49 +0100

    make Design Changes for the two views

--- app/src/main/java/com/example/levelfinder/IndexFragment.java
+++ app/src/main/java/com/example/levelfinder/IndexFragment.java
@@ -16,10 +16,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.levelfinder.R;
-
-import java.util.ArrayList;
+import com.example.levelfinder.databinding.FragmentFirstBinding;
 
 public class IndexFragment extends Fragment implements SensorEventListener {
 
@@ -27,8 +26,14 @@ public class IndexFragment extends Fragment implements SensorEventListener {
     private Sensor accelerometer;
     private TextView textView;
 
+    private TextView test;
+
     private GPSManager gpsLocationManager;
     private SharedPreferences sharedPreferences;
+
+    private Boolean done = false;
+    private FragmentFirstBinding binding;
+
 
     @Nullable
     @Override
@@ -37,15 +42,16 @@ public class IndexFragment extends Fragment implements SensorEventListener {
         gpsLocationManager = new GPSManager(getActivity());
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
         sensorManager = (SensorManager) getActivity().getSystemService(getActivity().SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        textView = view.findViewById(R.id.coordinates);
+        textView = binding.coordinates;
         textView.setText("Accelerometer:\n" + "X: " + 0 + "\nY: " + 0 + "\nZ: " + 0);
 
+        test = binding.test2;
 
         // start sensor listener
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
@@ -53,16 +59,47 @@ public class IndexFragment extends Fragment implements SensorEventListener {
         return view;
     }
 
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         float x = event.values[0];
         float y = event.values[1];
         float z = event.values[2];
 
+
         textView.setText("Accelerometer:\n" + "X: " + x + "\nY: " + y + "\nZ: " + z);
+
+
+        if ((x < 1 && x > -1 && y < 1 && y > -1 ) && done == false) {
+            getLocationData();
+            System.out.println("Location data saved");
+            // print location data
+            float latitude = sharedPreferences.getFloat("latitude", 0);
+            float longitude = sharedPreferences.getFloat("longitude", 0);
+            System.out.println("Latitude: " + latitude);
+            System.out.println("Longitude: " + longitude);
+            test.setText("Latitude: " + latitude + "\nLongitude: " + longitude);
+            done = true;
+        }
+        if (x > 1 || x < -1 || y > 1 || y < -1) {
+            done = false;
+        }
 
         // update chart
 
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // on places button click
+
+        binding.places.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(IndexFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
     }
 
     @Override
@@ -70,14 +107,11 @@ public class IndexFragment extends Fragment implements SensorEventListener {
         // not used
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
     }
-
-
 
     private void getLocationData() {
         gpsLocationManager.setOnLocationChangedListener(new GPSManager.OnLocationChangedListener() {
--- app/src/main/java/com/example/levelfinder/StorageFragment.java
+++ app/src/main/java/com/example/levelfinder/StorageFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,20 +19,19 @@ public class StorageFragment extends Fragment {
     private FragmentSecondBinding binding;
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the parent layout from the layout resource file
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        // Return the inflated view
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(StorageFragment.this)
